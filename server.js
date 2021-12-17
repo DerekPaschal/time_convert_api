1ada34d26e531058450597fb1ae43901761f3c21
Derek Paschal <dtpaschal@gmail.com>
2021-12-17 14:35:46 -0600

    JSON Endpoint
    Updated to use latest node version.
    Added json-returning endpoint with same functionality.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 
 // Constants
 const PORT = 80;
-console.log("PORT: "+PORT);
-const HOST = '0.0.0.0';
 
 const daysInMonth = 30.437;
 const daysInYear = 365.24;
@@ -26,37 +24,7 @@ app.get('/time-convert-xml', (req, res) => {
         // Amount
         let amount = req.query.amount;
 
-        if (convertFrom == null || convertTo == null || amount == null
-            || convertFrom == "" || convertTo == "" || amount == "") {
-
-             throw "Error converting: from="+convertFrom+"; to="+convertTo+"; amount="+amount;
-        }
-
-        convertFrom = convertFrom.toLowerCase().trim();
-        convertTo = convertTo.toLowerCase().trim();
-        amount = Number(amount.toLowerCase().trim());
-
-        let result = amount;
-
-        if (convertFrom == "year" || convertFrom == "years") {
-            result *= daysInYear;
-        } else if (convertFrom == "month" || convertFrom == "months") {
-            result *= daysInMonth;
-        } else if (convertFrom == "week" || convertFrom == "weeks") {
-            result *= 7;
-        } else if (convertFrom != "day" && convertFrom != "days") {
-            throw "Must convert from years, months, weeks, or days.";
-        }
-
-        if (convertTo == "year" || convertTo == "years") {
-            result /= daysInYear;
-        } else if (convertTo == "month" || convertTo == "months") {
-            result /= daysInMonth;
-        } else if (convertFrom == "week" || convertFrom == "weeks") {
-            result /= 7;
-        } else if (convertTo != "day" && convertTo != "days") {
-            throw "Must convert to years, months, weeks, or days.";
-        }
+        let result = Math.round(timeConvert(convertFrom, convertTo, amount));
 
         res.writeHead(200, {
             'Access-Control-Allow-Origin': '*',
@@ -65,7 +33,7 @@ app.get('/time-convert-xml', (req, res) => {
             'Content-Type': 'text/html'
         });
 
-        res.end('<result>'+Math.round(result)+'</result>');
+        res.end('<result>'+result+'</result>');
 
     } catch (e) {
         res.writeHead(500, {
@@ -81,5 +49,80 @@ app.get('/time-convert-xml', (req, res) => {
     }
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/time-convert-json', (req, res) => {
+    try {
+        // Get query params
+        // Convert from
+        let convertFrom = req.query.from;
+        // Convert to
+        let convertTo = req.query.to;
+        // Amount
+        let amount = req.query.amount;
+
+        let result = Math.round(timeConvert(convertFrom, convertTo, amount));
+
+        res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'X-Requested-With',
+            'Cache': 'no-cache',
+            'Content-Type': 'text/html'
+        });
+
+        res.end(JSON.stringify({
+            "result": result
+        }));
+
+    } catch (e) {
+        res.writeHead(500, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'X-Requested-With',
+            'Cache': 'no-cache',
+            'Content-Type': 'text/html'
+        });
+
+        console.error(e);
+
+        res.end(JSON.stringify({
+            "error": e
+        }));
+    }
+});
+
+function timeConvert(convertFrom, convertTo, amount) {
+    if (convertFrom == null || convertTo == null || amount == null
+        || convertFrom == "" || convertTo == "" || amount == "") {
+
+         throw "Error converting: from="+convertFrom+"; to="+convertTo+"; amount="+amount;
+    }
+
+    convertFrom = convertFrom.toLowerCase().trim();
+    convertTo = convertTo.toLowerCase().trim();
+    amount = Number(amount.toLowerCase().trim());
+
+    let result = amount;
+
+    if (convertFrom == "year" || convertFrom == "years") {
+        result *= daysInYear;
+    } else if (convertFrom == "month" || convertFrom == "months") {
+        result *= daysInMonth;
+    } else if (convertFrom == "week" || convertFrom == "weeks") {
+        result *= 7;
+    } else if (convertFrom != "day" && convertFrom != "days") {
+        throw "Must convert from years, months, weeks, or days.";
+    }
+
+    if (convertTo == "year" || convertTo == "years") {
+        result /= daysInYear;
+    } else if (convertTo == "month" || convertTo == "months") {
+        result /= daysInMonth;
+    } else if (convertFrom == "week" || convertFrom == "weeks") {
+        result /= 7;
+    } else if (convertTo != "day" && convertTo != "days") {
+        throw "Must convert to years, months, weeks, or days.";
+    }
+
+    return result;
+}
+
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
